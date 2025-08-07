3c28b32f10bcbd4a95045f22088c65c5bc09ddd0
Desmond-Fon <desmondfon@yahoo.com>
2025-08-07 13:23:01 +0100

    completed forms

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,18 +6,18 @@ type Props = {
 const Hero = ({title, description}: Props) => {
     return (
       <div className="hero flex flex-col text-center items-center h-[350px] pt-[38px]">
-        <div className="max-w-3xl flex flex-col text-center items-center">
+        <div className="w-[80%] lg:max-w-3xl flex flex-col text-center items-center">
           <div>
             <div className="flex justify-center items-center">
-                <div className="bg-[#33C36C40] w-fit text-2xl font-medium h-[60px] px-[70px] flex justify-center items-center rounded-[15px] text-[#33C36C]">
+                <div className="bg-[#33C36C40] w-fit text-lg lg:text-2xl font-medium h-[60px] px-[70px] flex justify-center items-center rounded-[15px] text-[#33C36C]">
                   HackJos
                 </div>
             </div>
-            <h1 className="text-[45px] leading-[55px] font-medium pt-5">
+            <h1 className="text-[32px] lg:text-[45px] leading-[55px] font-medium pt-5">
               {title}
             </h1>
           </div>
-          <p className="text-[#000000BF] text-2xl pt-[18px]">
+          <p className="text-[#000000BF] text-lg lg:text-2xl pt-[18px]">
             {description}
           </p>
         </div>
